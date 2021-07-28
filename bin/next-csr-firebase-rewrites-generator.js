895a9ab5d3f2de5a072abc662673324a91e905c5
hiroro-work <hiroro@sonicgarden.jp>
2021-07-28 20:11:13 +0900

    Dynamic Routes の rule はすべて更新されるように

--- bin/next-csr-firebase-rewrites-generator.js
+++ bin/next-csr-firebase-rewrites-generator.js
@@ -17,22 +17,26 @@ if (!hostingPublic) {
   exit(errCode);
 }
 
+const isDynamicRoute = (string) => {
+  return /\[[^[\]/]+\]/.test(string);
+};
+
 glob(pattern, (err, files) => {
   if (err) {
     console.error('error: failed to glob. err:', err);
     exit(errCode);
   }
 
-  const targetFiles = files.filter((_) => /\[[^[\]/]+\]/.test(_));
+  const targetFiles = files.filter((_) => isDynamicRoute(_));
   const destinations = targetFiles.map((_) => _.replace(hostingPublic, ''));
   const newRewrites = destinations.map((destination) => ({
     source: destination.replace('.html', '').replace(/\[([^[\]/]+)\]/g, ':$1'),
     destination,
   }));
-  const mergedRewrites = newRewrites.reduce(
-    (acc, cur) => (acc.some((_) => _.source === cur.source) ? acc : [...acc, cur]),
-    rewrites
-  );
+  const mergedRewrites = [
+    ...rewrites.filter(({ destination }) => !isDynamicRoute(destination)),
+    ...newRewrites,
+  ];
   const newConfig = {
     ...config,
     hosting: { ...hosting, rewrites: mergedRewrites, cleanUrls: true },
