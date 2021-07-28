e6ff382fed67fb021fa41ec617f0b0853c21f67a
hiroro-work <hiroro@sonicgarden.jp>
2021-07-29 06:40:37 +0900

    オプションチェックするように

--- bin/next-csr-rewrites-generate/index.js
+++ bin/next-csr-rewrites-generate/index.js
@@ -2,16 +2,53 @@
 'use strict';
 
 const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
 const { exit } = require('process');
+const { basename } = require('path');
+const { description } = require('../../package.json');
 
-const optionDefinitions = [{ name: 'format', type: String }];
+// eslint-disable-next-line no-undef
+const commandName = basename(__dirname);
+const optionDefinitions = [
+  {
+    name: 'format',
+    alias: 'f',
+    type: String,
+    typeLabel: '{underline firebase}',
+    description: 'Format type',
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Show usage',
+  },
+];
 const options = commandLineArgs(optionDefinitions);
-const { format } = options;
+const { format, help } = options;
+const sections = [
+  {
+    header: commandName,
+    content: description,
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions,
+  },
+];
+const usage = commandLineUsage(sections);
+
+if (help) {
+  console.info(usage);
+  exit(0);
+}
 
 try {
+  if (format !== 'firebase') throw new Error(`error: invalid format. value=${format}`);
+
   const { generate } = require(`./${format}`);
   generate();
 } catch (e) {
-  console.error(e);
+  console.error(e.message);
   exit(1);
 }
